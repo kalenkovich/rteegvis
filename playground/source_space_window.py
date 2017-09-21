57ffff97b48fa5ca862ff6f4d4962a2941c4a963
Evgenii <e.kalenkovich@gmail.com>
2017-09-21 15:27:38 +0300

    Changed source_space_window to use stream_file_in_a_thread

--- playground/source_space_window.py
+++ playground/source_space_window.py
@@ -6,7 +6,7 @@ from multiprocessing import Process
 from nfb.pynfb.windows import SourceSpaceWindow
 from nfb.pynfb.brain import (SourceSpaceRecontructor, SourceSpaceWidgetPainterSettings)
 from nfb.pynfb.io.xml_ import xml_file_to_params
-from nfb.pynfb.generators import run_eeg_sim
+from nfb.pynfb.generators import stream_file_in_a_thread
 from nfb.pynfb.inlets.lsl_inlet import LSLInlet
 
 protocol = SourceSpaceRecontructor(signals=None)
@@ -24,22 +24,21 @@ params = xml_file_to_params('nfb/pynfb/sourcespace.xml')
 file_path = params['sRawDataFilePath']
 stream_name = params['sStreamName']
 reference = params['sReference']
-raw = mne.io.read_raw_fif(file_path, verbose='ERROR')
-labels = raw.info['ch_names']
-fs = raw.info['sfreq']
-start, stop = raw.time_as_index([0, 60])  # read the first 15s of data
-source_buffer = raw.get_data(start=start, stop=stop)
-thread = Process(target=run_eeg_sim, args=(), kwargs={'chunk_size': 0, 'source_buffer': source_buffer,
-                                                      'name': stream_name, 'labels': labels, 'freq': fs})
+
+thread = stream_file_in_a_thread(file_path, reference, stream_name)
 thread.start()
 
 # Start updating the brain
 stream = LSLInlet(name=stream_name)
+freq = stream.get_frequency()
+
+
 def update():
     chunk = stream.get_next_chunk()
     if chunk is not None:
         protocol.widget_painter.redraw_state(chunk)
 
+
 timer = QtCore.QTimer()
 timer.timeout.connect(update)
-timer.start(1000. / fs)
+timer.start(1000. / freq)
