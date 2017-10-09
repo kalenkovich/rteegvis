d26b380ae921aff6176fd43104cfbd0f20777844
kalenkovich <e.kalenkovich@gmail.com>
2017-10-09 10:50:46 +0300

    Added shell scripts to stream mne sample and open source space vis

---
+++ shell_scripts/run_vis.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+source activate nfb_ubuntu
+cd ..
+export PYTHONPATH=.
+python playground/source_space_window.py
---
+++ shell_scripts/stream_sample.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+source activate nfb_ubuntu
+cd ..
+export PYTHONPATH=.
+python playground/stream_sample.py
