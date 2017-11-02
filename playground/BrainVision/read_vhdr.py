d0480f34a08de8b454a5a7cf48b3ff78b11376b6
kalenkovich <e.kalenkovich@gmail.com>
2017-11-02 13:24:23 +0300

    Working on reading brainvision files

--- playground/BrainVision/read_vhdr.py
+++ playground/BrainVision/read_vhdr.py
@@ -1,5 +1,7 @@
+import nfb.pynfb.io.brainvision as brainvision
 
 
+from nfb.pynfb.io.brainvision import read_raw_brainvision
 vhdr_path = r'/home/evgenii/Downloads/ExpData/Bulavenkova_A_2017-10-24_15-33-18_Rest.vhdr'
 
 brainvision_data = read_raw_brainvision(vhdr_fname=vhdr_path)
